3a88de2cf455f1461b170e60f947852f00b18fd4
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-01-14 08:27:26 -0700

    added a tab and a delay to the splash screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SplashScreenAnimation from "./animations/SplashScreenAnimation";
+import { Text, View } from "react-native";
+import texts from "./style_sheets/texts";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
+  // colorScheme accesses the users color preferences
   const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
@@ -14,6 +18,7 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
+        {/* Navigation is rendered after the splash*/}
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
---
+++ animations/path.js
@@ -0,0 +1,5 @@
+const { join } = require("path");
+
+const path = join(__dirname, "..", "assets", "images", "splash.png");
+
+console.log(path);
